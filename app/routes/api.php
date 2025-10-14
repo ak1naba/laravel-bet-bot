4dda2ca34abaa5496f1102a6ffb57c5c68af1af9
sergey kondratev <sergejkondratev985@gmail.com>
2025-10-14 11:23:56 +0500

    feat(logic): remove unused routes

--- app/routes/api.php
+++ app/routes/api.php
@@ -14,24 +14,4 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::group(['middleware'=>'auth:sanctum'], function (){
     Route::post('/logout', [AuthController::class, 'logout']);
-
-    Route::middleware([AdminAccess::class])->prefix('/admin')->group(function () {
-        Route::get('/users', [UserController::class, 'index']);
-        Route::post('/users', [UserController::class, 'create']);
-        Route::put('/users/{user}', [UserController::class, 'update']);
-        Route::delete('/users/{user}', [UserController::class, 'destroy']);
-    });
-
-    Route::middleware([WriterAccess::class])->prefix('/posts')->group(function () {
-        Route::get('/', [NewItemController::class, 'index']);
-        Route::post('/', [NewItemController::class, 'store']);
-        Route::middleware([NewItemOwning::class])->group(function () {
-            Route::get('/{new_item}', [NewItemController::class, 'show']);
-            Route::put('/{new_item}', [NewItemController::class, 'update']);
-            Route::delete('/{new_item}', [NewItemController::class, 'update']);
-        });
-    });
-
-    Route::get('/news', [NewItemController::class, 'getNewItems']);
-    Route::get('/news/{new_item}', [NewItemController::class, 'getNewItem']);
 });
