03708594893072ba6a6053e18204afd65df39778
Sergey Kondratev <sergejkondrattev985@gmail.com>
2025-10-20 00:03:51 +0500

    feat(telegram): attampt to fix https

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(TrustProxies::class);
+
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
